052c26605fe9b1e1372ea17aad2e99bd17f96fb8
Kevin Delord <delord.kevin@gmail.com>
2016-12-01 17:44:12 +0100

    Parse HTML using Kanna

--- PictureMyWorld/ViewController.swift
+++ PictureMyWorld/ViewController.swift
@@ -7,28 +7,56 @@
 //
 
 import UIKit
+import Kanna
 
 class ViewController: UIViewController {
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
 
+		if let html = self.fetchHTML() {
+			self.parseHTML(html)
+		}
+	}
+
+	private func fetchHTML() -> String? {
 		let myURLString = "http://picturemy.world"
 		guard let myURL = NSURL(string: myURLString) else {
 			print("Error: \(myURLString) doesn't seem to be a valid URL")
-			return
+			return nil
 		}
 
 		do {
 			let myHTMLString = try String(contentsOfURL: myURL)
-			print("HTML : \(myHTMLString)")
+			return myHTMLString
+
 		} catch let error as NSError {
 			print("Error: \(error)")
+			return nil
 		}
 	}
 
-	override func didReceiveMemoryWarning() {
-		super.didReceiveMemoryWarning()
-		// Dispose of any resources that can be recreated.
+	private func parseHTML(html: String) {
+
+		var posts = [[String:String]]()
+
+		if let doc = HTML(html: html, encoding: NSUTF8StringEncoding) {
+
+			// Search for nodes by XPath
+			for node in doc.xpath("//section/ul[@class='grid']/li/figure") {
+
+				var post = [String:String]()
+				post["thumbnailURL"] = node.xpath("img").first?["src"]
+				post["title"] = node.xpath("figcaption/h3").first?.text
+				post["googleMapsLink"] = node.xpath("figcaption/a").first?["href"]
+				post["googleMapsText"] = node.xpath("figcaption/a").first?.text
+				post["dateString"] = node.xpath("figcaption/p[@class='post-date']").first?.text
+				post["descriptionText"] = node.xpath("figcaption/p").first?.text
+
+				posts.append(post)
+			}
+		}
+
+		print(posts)
 	}
 }
