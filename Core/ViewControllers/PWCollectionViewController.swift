1e187af4bb960e39d6209fa919bfde8466f89fb8
Kevin Delord <delord.kevin@gmail.com>
2016-12-17 00:24:02 +0100

    Create LIVE target and rename ALPHA target

--- Core/ViewControllers/PWCollectionViewController.swift
+++ Core/ViewControllers/PWCollectionViewController.swift
@@ -73,11 +73,13 @@ extension PWCollectionViewController {
 	}
 
 	private func refreshContent(didPullToRefresh pullToRefresh: Bool = false) {
+		#if !ALPHA
 		guard (Reachability.isConnected == true) else {
 			self.showNoInternetAlert(didPullToRefresh: pullToRefresh)
 			self.refreshControl.endRefreshing()
 			return
 		}
+		#endif
 
 		let baseURL = NSBundle.stringEntryInPListForKey(PWPlist.APIBaseURL)
 		if let html = HTMLParser.fetchHTML(fromString: baseURL) {
